3e86fcfc75866262e07c12dacb92cdf3418c1e0c
iwaiwen@163.com <958159428@qq.com>
2017-12-24 11:38:09 +0800

    新建淘宝商品评论文件

---
+++ algorithm/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+
+#author:waiwen
+#email:iwaiwen@163.com
+#time: 2017/12/18 19:28
+
---
+++ taobao_spider/shop_rate.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+#author:waiwen
+#email:iwaiwen@163.com
+#time: 2017/12/24 11:35
+
+
+
+
